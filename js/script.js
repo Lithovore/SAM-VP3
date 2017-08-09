214a7dc1044d0e1ff63cc9e25d7466b310040c4d
Reuben Cox <reuben@example.com>
2017-08-10 02:58:41 +1200

    New style

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById('myDIV');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function rollover()
+{
+
+    whoissamroll.src = 'images/whoissam2.png';
+
+}
+function mouseaway()
+{
+
+    whoissamroll.src = 'images/whoissam1.png';
+
+}
