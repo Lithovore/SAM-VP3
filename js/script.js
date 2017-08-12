05a1564c536a3a1f80e0eff46941264c052a9f46
Reuben Cox <reuben@example.com>
2017-08-12 13:48:33 +1200

    NEW DIVS OMG.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-function myFunction() {
-    var x = document.getElementById('myDIV');
+window.onload=function(){
+  document.getElementById('iamsam').style.display = 'none';
+}
+
+function whoissam() {
+    var x = document.getElementById('iamsam');
     if (x.style.display === 'none') {
         x.style.display = 'block';
     } else {
         x.style.display = 'none';
     }
+
+    var y = document.getElementById('whoissam');
+    if (y.style.display === 'none') {
+        y.style.display = 'block';
+        y.fadein
+    } else {
+        y.style.display = 'none';
+    }
+
 }
 
 function rollover()
